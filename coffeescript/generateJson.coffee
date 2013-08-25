235119c9efaae0d4717597b74c3e37c40550bf3c
Jeff Lyon <jeff@albatrossdigital.com>
2013-08-25 09:52:35 -0700

    Rm old dirs

--- coffeescript/generateJson.coffee
+++
@@ -1,6 +0,0 @@
-
-`googleQuery = new GoogleSpreadsheetsQuery(filters, function(data) {
-  locache.set("blueGuideData", data);
-  query = new JsonQuery("body", data);
-});`
-googleQuery.get "select *"
--- js/generateJson.js
+++
@@ -1,9 +0,0 @@
-googleQuery = new GoogleSpreadsheetsQuery(filters, function(data) {
-  locache.set("blueGuideData", data);
-  query = new JsonQuery("body", data);
-});;
-googleQuery.get("select *");
-
-/*
-//@ sourceMappingURL=generateJson.js.map
-*/
